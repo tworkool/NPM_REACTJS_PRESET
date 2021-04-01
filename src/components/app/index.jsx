8e5d23d959d4478dc49f92b15053e3e2b5f53aae
Oliver Tworkowski <olitworkowski@gmail.com>
2021-04-01 19:19:17 +0200

    improve project structure

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-import './app.scss';
+import "./style.scss";
 
 const App = () => {
   return(
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-import App from './components/app';
+import App from "./components/app";
 
 ReactDOM.render(
   <App/>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
